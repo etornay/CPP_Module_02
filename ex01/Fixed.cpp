418657014c71797fb52be10ddacdcb82ebe11004
Elias Tornay Paz <etornay-@c3r10s6.42malaga.com>
2024-07-18 18:20:19 +0200

    CPP_Module_02

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,71 @@
 /*   By: etornay- <etornay-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 19:48:27 by etornay-          #+#    #+#             */
-/*   Updated: 2024/07/15 19:48:28 by etornay-         ###   ########.fr       */
+/*   Updated: 2024/07/18 17:16:10 by etornay-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	value = 0;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	value = copy.value;
+}
+
+Fixed::Fixed(const int i)
+{
+	std::cout << "Int constructor called" << std::endl;
+	value = (i << bits);
+}
+
+Fixed::Fixed(const float f)
+{
+	std::cout << "Float constructor called" << std::endl;
+	value = roundf(f * (1 << bits));
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	if (this != &other)
+		value = other.value;
+	std::cout << "Copy assignment operator called" << std::endl;
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (value);
+}
+void Fixed::setRawBits(int const raw)
+{
+	value = raw;
+}
+
+int Fixed::toInt(void) const
+{
+	return (value >> bits); /* Divide value entre 256 */
+}
+
+float Fixed::toFloat(void) const
+{
+	return static_cast<float>(value) / (1 << bits); /* (1 << bits) = 256 */
+}
+
+std::ostream &operator << (std::ostream &out, const Fixed &nbr)
+{
+	out << nbr.toFloat();
+	return (out);
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,27 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: etornay- <etornay-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/15 19:47:25 by etornay-          #+#    #+#             */
-/*   Updated: 2024/07/18 17:32:06 by etornay-         ###   ########.fr       */
+/*   Created: 2024/07/18 17:48:15 by etornay-          #+#    #+#             */
+/*   Updated: 2024/07/18 17:49:52 by etornay-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main(void)
+int main( void )
 {
-	Fixed a;
-	Fixed const b(10);
-	Fixed const c(42.42f);
-	Fixed const d(b);
-	a = Fixed(1234.4321f);
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	return (0);
+Fixed a;
+Fixed const b( Fixed(5.05f) * Fixed(2));
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+std::cout << b << std::endl;
+std::cout << Fixed::max(a, b) << std::endl;
+return (0);
 }
